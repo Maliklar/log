f431f9f413eacd4be85dfb581913c715e9cbdb4e
Malik Elbadri <elbadrimalik@gmail.com>
2024-04-13 15:40:28 +0300

    Updated version

--- test/index.js
+++ test/index.js
@@ -1,6 +1,10 @@
 import Log from "node-fancy-log";
 
-// Log.blink().log("HI");
+Log.blink().log("HI");
 Log.color("red").log("HI");
 Log.color("green").bgColor("green").bold().log("HI");
-console.log("\x1b[31m", "\x1b[44m", "HI", "\x1b[0m\x1b[0m");
+// console.log("\x1b[31m" + "\x1b[45m", "HI", "\x1b[0m", "\x1b[31m");
+// console.log("\x1b[33m%s\x1b[0m", "hi!");
+// const list = Log.createList();
+// list.add("sdfasdf");
+// list.log();
